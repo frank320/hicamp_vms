147e2232bb46c7e6928badb17552831327feb21d
frank@tuxing <zhangshipeng@tuxing2010.com>
2017-05-16 15:39:25 +0800

    添加一个导出excel表格的页面

---
+++ src/backend/api/getAll.js
@@ -0,0 +1,31 @@
+/**
+ * Created by frank on 2017/5/16.
+ */
+/**
+ * Created by wikeLi on 2017/4/25.
+ */
+const router = require('express').Router()
+const Album = require('../model/album')
+
+/*
+ * GET
+ * 获取资源列表
+ * */
+router.get('/getAll', (req, res)=> {
+  Album
+    .find({})
+    .sort({createdTime: -1})
+    .then(r=> {
+      res.json({
+        code: 200,
+        data: r
+      })
+    })
+    .catch(err=> {
+      res.json({
+        code: 500,
+        data: err
+      })
+    })
+})
+module.exports = router
