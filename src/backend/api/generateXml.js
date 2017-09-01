f652cbe201ee5a6e3195e4f13e49e4571d2e51b1
frank@tuxing <zhangshipeng@tuxing2010.com>
2017-09-01 10:38:55 +0800

    修改文件下载协议为http协议

--- src/backend/api/generateXml.js
+++ src/backend/api/generateXml.js
@@ -18,7 +18,7 @@ template.config('base', path.join(__dirname, '../template'))
 router.post('/generateXml', (req, res)=> {
   const id = req.body.id
   const currentTime = moment(+new Date()).format('YYYY-MM-DD HH:mm:ss')
-  const fileServer = 'ftp://test:test@101.200.84.44:21/'
+  const fileServer = 'http://101.200.84.44:80/bo/download/TJ/'
   return Album
     .findOne({id: id})
     .then(data=> {
